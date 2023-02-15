1c78ec0d1a4343ecf4b53fd2b8ae2b7439737db5
B-G-M <71920516+B-G-M@users.noreply.github.com>
2023-02-15 18:27:42 +0300

    upd

--- src/modules/ApproachControl/ApproachControl/Program.cs
+++ src/modules/ApproachControl/ApproachControl/Program.cs
@@ -1,91 +1,93 @@
 ﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
+using System.Threading.Channels;
 using System.Xml.Linq;
 
 namespace ApproachControl
 {
 	internal class Program
 	{
-        static void Receive(string message)
-        {
-            if(message == "Start")
-            {
-                Console.WriteLine("Вау");
-            }
-        }
-
-        static void Main(string[] args)
+		class Rabbit
 		{
-
-
-			// Подключение к RabbitMo
-			var factory = new ConnectionFactory
+			public Rabbit() 
 			{
-				HostName = "178.20.43.80",
-				UserName = "guest",
-				Password = "guest",
-				VirtualHost = "/",
-				Port = 5672
-			};
-			var connection = factory.CreateConnection();
-            var channel = connection.CreateModel();
-            string name = "Approach Control";
+				// Подключение к RabbitMo
+				var factory = new ConnectionFactory
+				{
+					HostName = "178.20.43.80",
+					UserName = "guest",
+					Password = "guest",
+					VirtualHost = "/",
+					Port = 5672
+				};
+				var connection = factory.CreateConnection();
+				channel = connection.CreateModel();
+				
+				// Создание очереди
+				channel.QueueDeclare(
+					queue: name,
+					durable: true,
+					exclusive: false,
+					autoDelete: true,
+					arguments: null
+					);
 
-            // Создание очереди
-            channel.QueueDeclare(
-                queue: name,
-                durable: true,
-                exclusive: false,
-                autoDelete: true,
-                arguments: null
-                );
+				//Подключение к exchange вашего модуля
+				channel.QueueBind(
+					exchange: name,
+					queue: name,
+					routingKey: name
+					);
 
-            // Слушаем очередь
-            var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += (model, ea) =>
-            {
-                var body = ea.Body.ToArray();
-                var message = Encoding.UTF8.GetString(body);
-                Receive(message);
-                Console.WriteLine($" [x] Received {message}");
-            };
-            channel.BasicConsume(queue: "Visualizer",
-                                 autoAck: true,
-                                 consumer: consumer);
-            // Создаем exchange
-            channel.ExchangeDeclare(
-            exchange: name,
-            type: "fanout",
-            durable: true,
-            autoDelete: true,
-            arguments: null);
+				//Подключение к global exchange
+				channel.QueueBind(
+					exchange: "global",
+					queue: name,
+					routingKey: name
+					);
+			}
+			IModel channel;
+			string name = "Approach Control";
 
-            //Подключение к exchange вашего модуля
-            channel.QueueBind(
-                exchange: name,
-                queue: name,
-                routingKey: name
-				);
+			public void Send(string message, string direction)
+			{
+				//Отправка сообщения Visualizer, означающее, что вы подключились
+				var body = Encoding.UTF8.GetBytes(message);
+				channel.BasicPublish(
+					exchange: direction,
+						routingKey: direction,
+						basicProperties: null,
+						body: body);
+			}
 
-            //Подключение к global exchange
-            channel.QueueBind(
-                exchange: "global",
-                queue: name,
-                routingKey: name
-                );
+			public string Receive(string direction = "Visualizer")
+			{
+				// Слушаем очередь
+				var consumer = new EventingBasicConsumer(channel);
+				string message = "";
+				consumer.Received += (model, ea) =>
+				{
+					var body = ea.Body.ToArray();
+					message = Encoding.UTF8.GetString(body);
+					Console.WriteLine($" [x] Received {message}");
+				};
+				channel.BasicConsume(queue: direction,
+											autoAck: true,
+											consumer: consumer);
+				return message;
+			}
+			
+		}
 
-            //Отправка сообщения Visualizer, означающее, что вы подключились
-            string message = name;
-            var body = Encoding.UTF8.GetBytes(message);
-            channel.BasicPublish(
-                exchange: "Visualizer",
-                routingKey: "Visualizer",
-                basicProperties: null,
-                body: body);
+		static void Main(string[] args)
+		{
+			Rabbit rabbit = new Rabbit();
+			rabbit.Send("Approach Control", "Visualizer");
+			
 
 
-        }
+		}
 
 
 	}
