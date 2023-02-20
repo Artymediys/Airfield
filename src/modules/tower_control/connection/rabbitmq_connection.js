a8b20459a8e54964cb26e0f022ea8ad79875f749
Artymediys <Artymediys@yandex.ru>
2023-02-20 15:58:31 +0300

    fixed rabbitmq

--- src/modules/tower_control/connection/rabbitmq_connection.js
+++ src/modules/tower_control/connection/rabbitmq_connection.js
@@ -1,6 +1,7 @@
 const amqp = require("amqplib");
 
-const {Connection, Exchange, Queue} = require("config.json");
+const {connection: Connection, exchange: Exchange, queue: Queue} = require("./config.json");
+
 
 function getMessage(channel, queue) {
     return new Promise(resolve => {
@@ -15,21 +16,42 @@ async function run() {
         username: Connection.username,
         password: Connection.password,
     });
+    console.log("Connected to RabbitMQ server!")
 
     const channel = await connection.createChannel();
+    console.log("Channel created!")
+
+
+    process.on('SIGINT', () => {
+        channel.close();
+        connection.close();
+        console.info(' -> Client finished!');
+    });
+
 
     await channel.assertExchange(Exchange.name, Exchange.type);
-    const twQueue = await channel.assertQueue(Queue);
-    await channel.bindQueue(twQueue.queue, Exchange.name, "");
+    console.log("Exchange asserted!")
 
-    channel.sendToQueue("Visualizer", Buffer.from(`${Exchange.name} connected`));
+    const twQueue = await channel.assertQueue(Queue);
+    console.log("Exchange asserted!")
+
+    await channel.bindQueue(twQueue.queue, Exchange.name, "");
+    await channel.bindQueue(twQueue.queue, "global", "");
+    console.log("Queues bound!")
+
+    /*
+    channel.sendToQueue("Visualizer", Buffer.from(`${Exchange.name}`));
+    console.log("Message sent to Visualizer queue!")
+     */
+
+    channel.publish("Visualizer", "", Buffer.from(`${Exchange.name}`));
+    console.log("Message sent to Visualizer exchanger!")
 
     const receivedMessage = await getMessage(channel, twQueue.queue);
+    console.log(`Message received: ${receivedMessage.content.toString()}`);
 
     await channel.ack(receivedMessage);
-
-    await channel.close();
-    await connection.close();
+    console.log("Acknowledged!");
 }
 
-run().then(() => console.log("Finished"));
+run().then(() => console.log("\nClient started!"));
